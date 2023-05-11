f4a3c0d1b3c139da3b518ff36c29fed718e8ced6
byeol <codbyeol@gmail.com>
2023-05-11 22:34:23 +0900

    오류수정

--- src/main/java/com/app/oc/service/SearchService.java
+++ src/main/java/com/app/oc/service/SearchService.java
@@ -25,20 +25,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SearchService {
 
-    // 썸네일 추가 작업 필요
     // 지역(시,구,동 / City,District,Neighborhood 필터링 작업 추가)
 
     private FileRepository fileRepository;
     private ItemRepository itemRepository;
 
 
+
+    //카테고리
     @Transactional
     public ItemPageDto getItemByCategory(Integer category, Pageable pageable){
         ItemPageDto itemPageDto = new ItemPageDto();
         //item List
         List<Item> getItems = itemRepository.searchByCategory(category, pageable);
-
-        //id들로만 한번에
         List<Long> Ids = getItems.stream().map(item -> item.getItemId()).collect(Collectors.toList());
 
         List<File> fileIn = fileRepository.findFileIn(Ids);
@@ -46,7 +45,7 @@ public class SearchService {
 
         Map<Long, UploadFile> fileMap = new HashMap<>();
         for (UploadFile uploadFile : uploadFiles) {
-            if (uploadFile.getStoreFileName().startsWith("s_")) { //썸네일만
+            if (uploadFile.getStoreFileName().startsWith("s_")) { //썸네일
                 fileMap.put(uploadFile.getItem_seq(), uploadFile);
             }
         }
@@ -60,6 +59,8 @@ public class SearchService {
         int startPage = Math.max(1, searchDtos.getPageable().getPageNumber() - 1);
         int endPage = Math.min(searchDtos.getTotalPages(), searchDtos.getPageable().getPageNumber()+3);
 
+
+        //페이지 정보
         itemPageDto.setItems(searchDtos);
         itemPageDto.setStartPage(startPage);
         itemPageDto.setEndPage(endPage);
@@ -67,22 +68,14 @@ public class SearchService {
         return itemPageDto;
     }
 
-    private static List<SearchDto> getsearchDtos(List<Item> getItems, Map<Long, UploadFile> fileMap) {
-        return getItems.stream().map(item -> {
-            SearchDto searchDto = new SearchDto();
-            searchDto.setUploadFile(fileMap.get(item.getItemId()));
-            return searchDto;
-        }).collect(Collectors.toList());
-    }
 
-
+    //키워드
     @Transactional
     public ItemPageDto getItemByKeyword(String keyword, Pageable pageable){
         ItemPageDto itemPageDto = new ItemPageDto();
+
         //item List
         List<Item> getItems = itemRepository.searchByKeyword(keyword, pageable);
-
-        //id들로만 한번에
         List<Long> Ids = getItems.stream().map(item -> item.getItemId()).collect(Collectors.toList());
 
         List<File> fileIn = fileRepository.findFileIn(Ids);
@@ -90,7 +83,7 @@ public class SearchService {
 
         Map<Long, UploadFile> fileMap = new HashMap<>();
         for (UploadFile uploadFile : uploadFiles) {
-            if (uploadFile.getStoreFileName().startsWith("s_")) { //썸네일만
+            if (uploadFile.getStoreFileName().startsWith("s_")) { //썸네일
                 fileMap.put(uploadFile.getItem_seq(), uploadFile);
             }
         }
@@ -104,6 +97,8 @@ public class SearchService {
         int startPage = Math.max(1, searchDtos.getPageable().getPageNumber() - 1);
         int endPage = Math.min(searchDtos.getTotalPages(), searchDtos.getPageable().getPageNumber()+3);
 
+
+        //페이지 정보
         itemPageDto.setItems(searchDtos);
         itemPageDto.setStartPage(startPage);
         itemPageDto.setEndPage(endPage);
@@ -111,7 +106,7 @@ public class SearchService {
         return itemPageDto;
     }
 
-    private static List<SearchDto> getkeyworditem(List<Item> getItems, Map<Long, UploadFile> fileMap) {
+    private static List<SearchDto> getsearchDtos(List<Item> getItems, Map<Long, UploadFile> fileMap) {
         return getItems.stream().map(item -> {
             SearchDto searchDto = new SearchDto();
             searchDto.setUploadFile(fileMap.get(item.getItemId()));
