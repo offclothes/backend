dfe3fe6c30ee8f70338dac972e8f73bce33c97c8
rinklove <88640680+rinklove@users.noreply.github.com>
2023-05-23 20:16:14 +0900

    chore: 코드 컨플릭트 해결

--- src/test/java/com/app/oc/controller/ShoppingMalControllerTest.java
+++ src/test/java/com/app/oc/controller/ShoppingMalControllerTest.java
@@ -35,11 +35,8 @@ class ShoppingMalControllerTest {
     @Test
     public void findMyshop() throws Exception{
     //given
-<<<<<<< HEAD
 //         shoppingMalController.MyShopping(1L);
-=======
 
->>>>>>> 4c8e3d6180a67b6e5ab319d5f6a340110a6c35c4
 
     //when
 
