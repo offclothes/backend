501f102b3e5f7df3d3b5e0c33fc3b13104533b02
jacomyou1026 <jacomyou1026@naver.com>
2023-08-14 03:18:57 +0900

    reset

--- src/main/java/com/app/oc/OcApplication.java
+++ src/main/java/com/app/oc/OcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OcApplication {
+class OcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcApplication.class, args);
